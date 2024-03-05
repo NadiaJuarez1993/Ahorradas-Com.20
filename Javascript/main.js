6990be43eee3eaffe2ee8f538619ac94556d4199
Nadia Juarez <nadiajuarezz@hotmail.com>
2024-03-05 18:07:38 -0300

    adding id generation function and function to hide and show dom elements

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,3 +1,18 @@
 /* UTILITIES */
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
+
+//GENERA id DINAMICO EN CADA USO
+const randomId = () => self.crypto.randomUUID();
+
+//FUNCION DE MOSTRAR Y OCULTAR ELEMENTOS DEL DOM
+const showElement = (selectors) => {
+  for (const selector of selectors) {
+    $(selector).classList.remove("hidden");
+  }
+};
+const hideElement = (selectors) => {
+  for (const selector of selectors) {
+    $(selector).classList.add("hidden");
+  }
+};
